b4e199089cc660a4b85a187f1f3b49d8f6b8501b
engRana404 <RanaGamalDaif@gmail.com>
2026-05-05 03:19:43 +0300

    Update WaitUtils to use configurable timeout; modify wait methods to return void for improved clarity

--- src/main/java/com/qaplayground/utils/WaitUtils.java
+++ src/main/java/com/qaplayground/utils/WaitUtils.java
@@ -2,25 +2,24 @@ package com.qaplayground.utils;
 
 import com.qaplayground.base.DriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class WaitUtils {
-    private static final int TIMEOUT_SECONDS = 10;
+    private static final int TIMEOUT_SECONDS = Integer.parseInt(ConfigReader.getProperty("timeout"));
 
     public static WebDriverWait getWait() {
         return new WebDriverWait(DriverFactory.getDriver(), Duration.ofSeconds(TIMEOUT_SECONDS));
     }
 
-    public static WebElement waitForElementVisible(By locator) {
-        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    public static void waitForElementVisible(By locator) {
+        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    public static WebElement waitForElementClickable(By locator) {
-        return getWait().until(ExpectedConditions.elementToBeClickable(locator));
+    public static void waitForElementClickable(By locator) {
+        getWait().until(ExpectedConditions.elementToBeClickable(locator));
     }
 
     public static void waitForUrlContains(String text) {
